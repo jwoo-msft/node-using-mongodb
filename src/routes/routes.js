83a647dac7ef57b2a9d457d30fad37d3f26098e7
Joseph Woo <Joseph.Woo@microsoft.com>
2022-09-30 18:14:41 -0700

    module2

---
+++ src/routes/routes.js
@@ -0,0 +1,9 @@
+import {addnewProduct} from '../controllers/controllers'
+
+const routes = (app) => {
+    app.route('/products')
+    // Post endpoint
+    .post(addnewProduct);
+}
+
+export default routes;
